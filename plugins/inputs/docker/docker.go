f021da1feaeadd974619f287c16cf615b99539aa
Cameron Sparr <cameronsparr@gmail.com>
2016-05-04 14:49:59 -0600

    Default docker timeout in case one is not defined in config

--- plugins/inputs/docker/docker.go
+++ plugins/inputs/docker/docker.go
@@ -466,6 +466,8 @@ func parseSize(sizeStr string) (int64, error) {
 
 func init() {
 	inputs.Add("docker", func() telegraf.Input {
-		return &Docker{}
+		return &Docker{
+			Timeout: internal.Duration{Duration: time.Second * 5},
+		}
 	})
 }
