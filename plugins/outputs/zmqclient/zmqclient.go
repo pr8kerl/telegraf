9abbd323e9530c50a90a307c40b391f23f57cf02
IanS <goodbloke@gmail.com>
2016-05-16 22:19:04 +1000

    updated zmqclient

--- plugins/outputs/zmqclient/zmqclient.go
+++ plugins/outputs/zmqclient/zmqclient.go
@@ -12,15 +12,31 @@ import (
 )
 
 type ZMQClient struct {
-	writer  io.Writer
-	closers []io.Closer
+	closers     []io.Closer
+	Zmqendpoint string
+	Zmqsvc      string
 
-	zmqsvc string
 	ctx    *zmq.Context
 	poller *zmq.Poller
 	connxn *zmq.Socket
 
 	serializer serializers.Serializer
+}
+
+func NewZMQClient() *ZMQClient {
+
+	tctx, err := zmq.NewContext()
+	if err != nil {
+		return nil
+	}
+	tconnxn, err := tctx.NewSocket(zmq.DEALER)
+	if err != nil {
+		return nil
+	}
+
+	return (&ZMQClient{
+		ctx:    tctx,
+		connxn: tconnxn})
 }
 
 var sampleConfig = `
@@ -39,13 +55,13 @@ func (z *ZMQClient) SetSerializer(serializer serializers.Serializer) {
 	z.serializer = serializer
 }
 
-func (z *ZMQClient) Write(message []byte) (n int, err error) {
+func (z *ZMQClient) Send(message []byte) (n int, err error) {
 
 	n, err = z.connxn.Send("", zmq.SNDMORE)
 	if err != nil {
 		return n, err
 	}
-	z.connxn.Send(z.zmqsvc, zmq.SNDMORE)
+	z.connxn.Send(z.Zmqsvc, zmq.SNDMORE)
 	if err != nil {
 		return n, err
 	}
@@ -62,19 +78,15 @@ func (z *ZMQClient) Close() error {
 }
 
 func (z *ZMQClient) Connect() error {
-	writers := []io.Writer{}
-	writers = append(writers, z)
-	z.writer = io.MultiWriter(writers...)
 	z.closers = append(z.closers, z)
 
-	ident := os.Hostname()
+	ident, err := os.Hostname()
+	if err != nil {
+		ident = "unknown"
+	}
 	z.connxn.SetIdentity(ident)
-	return z.connxn.Connect(z.zmqendpoint) // address of zmqBroker
+	return z.connxn.Connect(z.Zmqendpoint) // address of zmqBroker
 
-}
-
-func (z *ZMQClient) Close() error {
-	return z.connxn.Close()
 }
 
 func (z *ZMQClient) SampleConfig() string {
@@ -97,7 +109,7 @@ func (z *ZMQClient) Write(metrics []telegraf.Metric) error {
 		}
 
 		for _, value := range values {
-			_, err = z.writer.Write([]byte(value + "\n"))
+			_, err = z.Send([]byte(value + "\n"))
 			if err != nil {
 				return fmt.Errorf("FAILED to write message: %s, %s", value, err)
 			}
@@ -108,6 +120,6 @@ func (z *ZMQClient) Write(metrics []telegraf.Metric) error {
 
 func init() {
 	outputs.Add("zmqclient", func() telegraf.Output {
-		return &ZMQClient{}
+		return NewZMQClient()
 	})
 }
